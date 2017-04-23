b5a694f9a19f9de7c94eb63a4497f079d5053baf
robotcomputer <robotcomputer>
2017-04-22 17:51:44 -0700

    Finished map navigator

--- map_annotator/src/map_annotator.py
+++ map_annotator/src/map_annotator.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 import pickle
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 
 POSE_FILE = 'poses'
+SUB_NAME = 'amcl_pose'
+PUB_NAME = 'move_base_simple/goal'
 
 def help():
 	print 'Welcome to the map annotator!'
@@ -14,26 +18,61 @@ def help():
 	print "\tquit: Exits the program."
 	print "\thelp: Show this list of commands"
 
-def read_in_poses():
-	try:
-		with open(POSE_FILE, 'rb') as file:
-			poses = pickle.load(file)
-			return poses
-	except IOError:
-		return {}
+class PoseController(object):
+	def __init__(self):
+		self._pose_sub = rospy.Subscriber(SUB_NAME,
+										  PoseWithCovarianceStamped, 
+										  callback=self._pose_callback)
+		self._pose_pub = rospy.Publisher(PUB_NAME,
+										 PoseStamped,
+										 queue_size=10)
+		self._poses = self._read_in_poses()
+		self._curr_pose = None
 
-def write_out_poses(poses):
-	with open(POSE_FILE, 'wb') as file:
-		pickle.dump(poses, file)
+	def __str__(self):
+		if self._poses:
+			return "Poses:\n" "\n".join(["{}:\n{}".format(name, pose) for name, pose in self._poses.items()])
+		else:
+			return "No poses"
 
-def get_curr_pose():
-	return "Lelillelol"
+	def _read_in_poses(self):
+		try:
+			with open(POSE_FILE, 'rb') as file:
+				return pickle.load(file)
+		except IOError:
+			return {}
 
-def move_to_pose(pose, poses):
-	if pose in poses:
-		pass
+	def _write_out_poses(self):
+		with open(POSE_FILE, 'wb') as file:
+			pickle.dump(self._poses, file)
 
-def prompt(poses):
+	def _pose_callback(self, msg):
+		self._curr_pose = msg
+
+	def save_pose(self, pose_name):
+		if not self._curr_pose:
+			print "No pose available"
+			return
+		print "Saving pose {} as current position".format(pose_name)
+		self._poses[pose_name] = self._curr_pose
+		self._write_out_poses()
+
+	def delete_pose(self, pose_name):
+		if pose_name in self._poses:
+			del self._poses[pose_name]
+			print "Pose {} deleted".format(pose_name)
+			self._write_out_poses()
+		else:
+			print "Pose name {} does not exist".format(pose_name)
+
+	def move_to_pose(self, pose_name):
+		if pose_name in self._poses:
+			msg = PoseStamped()
+			msg.header = self._poses[pose_name].header
+			msg.pose = self._poses[pose_name].pose.pose
+			self._pose_pub.publish(msg)
+
+def prompt(pose_ctrl):
 	command = raw_input("> ").strip().split()
 	if not 1 <= len(command) <= 2:
 		print "Invalid command"
@@ -41,42 +80,33 @@ def prompt(poses):
 	if len(command) == 1:
 		command = command[0]
 	if len(command) == 2:
-		command, name = command
+		command, pose_name = command
 
 	if command == "list":
-		if poses:
-			print "Poses: {}".format(poses)
-		else:
-			print "No poses"
-
+		print str(pose_ctrl)
 	elif command == "save":
-		poses[name] = get_curr_pose()
-		write_out_poses(poses)
-
+		pose_ctrl.save_pose(pose_name)
 	elif command == "delete":
-		if name in poses:
-			del poses[name]
-		else:
-			print "Pose name {} does not exist".format(name)
-		write_out_poses(poses)
-
+		pose_ctrl.delete_pose(pose_name)
 	elif command == "goto":
-		move_to_pose(name, poses)
-
+		pose_ctrl.move_to_pose(pose_name)
 	elif command == "quit" or command == "q":
 		exit(0)
-
 	elif command == "help":
 		help()
-
 	else:
 		print "Invalid command"
 
+def wait_for_time():
+	while rospy.Time().now().to_sec() == 0:
+		pass
+
 if __name__ == '__main__':
+	rospy.init_node('pose_demo', disable_signals=True)
+	pose_ctrl = PoseController()
 	help()
 	while True:
-		poses = read_in_poses()
 		try:
-			prompt(poses)
+			prompt(pose_ctrl)
 		except (KeyboardInterrupt, SystemExit):
 			exit(0)
