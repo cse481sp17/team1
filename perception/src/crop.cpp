7d1a6171f8624ff6cc22d2f44c718cd97d612bdb
Jesse Liston <listonjesse@yahoo.com>
2017-05-16 14:20:30 -0700

    added transform to crop.cpp

--- perception/src/crop.cpp
+++ perception/src/crop.cpp
@@ -3,6 +3,12 @@
 #include "pcl/filters/crop_box.h"
 #include "pcl/common/common.h"
 
+#include "ros/ros.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "pcl_ros/transforms.h"
+#include "tf/transform_listener.h"
+#include "rosbag/bag.h"
+
 typedef pcl::PointXYZRGB PointC;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudC;
 
@@ -10,8 +16,29 @@ namespace perception {
   Cropper::Cropper(const ros::Publisher& pub) : pub_(pub) {}
 
   void Cropper::Callback(const sensor_msgs::PointCloud2& msg) {   
+      
+
+  // here we want to check the frame_id of the pointcloud
+  // and do a transformation to the base_link frame  
+  tf::TransformListener tf_listener;
+  tf_listener.waitForTransform("base_link", msg.header.frame_id,
+                                ros::Time(0), ros::Duration(5.0));
+  tf::StampedTransform transform;
+  try {
+    tf_listener.lookupTransform("base_link", msg.header.frame_id,
+                                ros::Time(0), transform);
+  } catch (tf::LookupException& e) {
+      std::cerr << e.what() << std::endl;
+      return;
+  } catch (tf::ExtrapolationException& e) {
+      std::cerr << e.what() << std::endl;
+      return;
+  }
+        
+  sensor_msgs::PointCloud2 cloud_transform_msg;
+  pcl_ros::transformPointCloud("base_link", transform, msg, cloud_transform_msg);
     PointCloudC::Ptr cloud(new PointCloudC());
-    pcl::fromROSMsg(msg, *cloud);
+    pcl::fromROSMsg(cloud_transform_msg, *cloud);
 
     PointCloudC::Ptr cropped_cloud(new PointCloudC());
     double min_x, min_y, min_z, max_x, max_y, max_z;
