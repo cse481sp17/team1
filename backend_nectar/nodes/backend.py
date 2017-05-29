e0bc609f81cb19c48afa9085f2332c216a881a95
Jesse Liston <listonjesse@yahoo.com>
2017-05-28 18:05:29 -0700

    removed TODOs from backend.py

--- backend_nectar/nodes/backend.py
+++ backend_nectar/nodes/backend.py
@@ -5,8 +5,6 @@ from perception_msgs.srv import Serving, ServingResponse, ServingRequest
 from navigator_msg.srv import Navigation, NavigationRequest, NavigationResponse
 from order_msgs.msg import Order
 
-#TODO import msg from backend_nectar, figure out how
-
 OrderLocationToNavigationLocation = {Order.COUNTER_AREA_1: NavigationRequest.COUNTER_AREA_1, Order.COUNTER_AREA_2: NavigationRequest.COUNTER_AREA_2}
 
 class NectarBackend:
@@ -20,17 +18,14 @@ class NectarBackend:
         rospy.wait_for_service('navigation') # service for navigation
         # use rossrv show navigator_msg/Navigator to see the possible call values
         self._navigator_server = rospy.ServiceProxy('navigation', Navigation)
-
-        # TODO setup all the other stuff with the frontend communication
+       
         # _order_callback is the callback for the frontend's topic
         # make a subscriber to the frontend
         self._frontend_sub = rospy.Subscriber('/orders', Order, self._order_callback)
 
-        #TODO make a publisher to the frontend to send errors
+        # make a publisher to the frontend to send errors
         self._frontend_error_pub = rospy.Publisher('/order_failure', Order, queue_size=10)
 
-
-    # TODO hook this callback into the frontend
     def _order_callback(self, msg):
         self._order_queue.append(msg)
 
@@ -42,32 +37,31 @@ class NectarBackend:
         self.run_order(cur_order)
 
     def run_order(self, order_msg):
-        #TODO run each step of the order_msg
-
         # go to the chef table
         ret = self._navigator_server(NavigationRequest.CHEF_TABLE)
         if not ret.success:
             self.error("ERROR on navigation to chef table", order_msg)
             return
 
+        # retreive the tray
         ret = self._serving_server(ServingRequest.RETREIVE)
         if not ret.success:
             self.error("ERROR in pick up tray", order_msg)
             return
 
-        # TODO, the navigation location comes from the frontend
+        # move to the customer
         ret = self._navigator_server(OrderLocationToNavigationLocation[order_msg.location])
         if not ret.success:
             self.error("ERROR in navigation to customer", order_msg)
             return
 
+        # place the tray
         ret = self._serving_server(ServingRequest.PLACE)
         if not ret.success:
             self.error("ERROR in placing tray", order_msg)
             return
 
-        #TODO communicate to the front end that the order_msg is done
-
+        # go back to the chef table
         ret = self._navigator_server(NavigationRequest.CHEF_TABLE)
         if not ret.success:
             self.error("ERROR on navigation to chef table", order_msg)
