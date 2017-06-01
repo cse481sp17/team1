6e33f7b3ae378d9b62c251dd6ad1d338c2e0fa7f
robotcomputer <robotcomputer>
2017-05-31 20:13:06 -0700

    added head tilt to services

--- map_annotator/nodes/NavigationServerNode.py
+++ map_annotator/nodes/NavigationServerNode.py
@@ -2,10 +2,12 @@
 from navigator_msg.srv import Navigation, NavigationRequest
 import rospy
 from pose_ctrl import PoseController
+import fetch_api
 
 class Navigator:
     def __init__(self):
         self._pose_ctrl = PoseController()
+        self._head = fetch_api.Head()
 
     # TODO: program the navigation with cli.py
     def navigate(self, req):
@@ -16,8 +18,10 @@ class Navigator:
         if name not in self._pose_ctrl._poses:
             print("{} program does not exist".format(name))
             return False
-        #TODO have run_program return a boolean
-        # false if the program could not run
+
+        # set the head to the ceiling
+        self._head.pan_tilt(0, fetch_api.Head.MIN_TILT)
+
         return self._pose_ctrl.move_to_pose(name)
 
 
--- perception/nodes/ServingServerNode.py
+++ perception/nodes/ServingServerNode.py
@@ -4,6 +4,7 @@ import rospy
 from program_ctrl import ProgramController
 
 class Server:
+    PAN_ANGLE = 0.642
     def __init__(self):
         self._program_ctrl = ProgramController()
 
@@ -21,6 +22,9 @@ class Server:
         if name not in self._program_ctrl._programs:
             print("{} program does not exist".format(name))
             return False
+
+        self._head.pan_tilt(0, PAN_ANGLE)
+
         return self._program_ctrl.run_program(name)
 
 
